31ff420d23af07514da1ade520ab6a37770807db
Jan Tacakiewicz <Epim3dium@protonmail.com>
2024-03-11 18:17:04 +0100

    different debug::log formatting

--- src/debug/log.cpp
+++ src/debug/log.cpp
@@ -7,8 +7,8 @@ std::string FormatTime(std::chrono::system_clock::time_point tp) {
     auto tp2 = std::chrono::system_clock::from_time_t(t);
     if (tp2 > tp)
         t = std::chrono::system_clock::to_time_t(tp - std::chrono::seconds(1));
-    ss  << std::put_time(std::localtime(&t), "%Y-%m-%d %T")
-        << "." << std::setfill('0') << std::setw(3)
+    ss  << std::put_time(std::localtime(&t), "%T") //"%Y-%m-%d %T"
+        << "." << std::setfill('0') << std::setw(6)
         << (std::chrono::duration_cast<std::chrono::microseconds>(
            tp.time_since_epoch()).count() % 1000000) ;
     return ss.str();
