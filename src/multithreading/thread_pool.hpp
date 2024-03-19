0f824e8aa1bae2f4bbab42de8365965932b717a5
Epim3dium <Epim3dium@protonmail.com>
2024-03-19 15:04:26 +0100

    mutithreading less cpu intesnsive (conditional variable instead of spin lock)

--- src/multithreading/thread_pool.hpp
+++ src/multithreading/thread_pool.hpp
@@ -14,6 +14,7 @@ class TaskQueue {
     std::queue< std::function<void()> > m_tasks;
     std::mutex m_mutex;
     std::atomic<size_t> m_remaining_tasks = 0;
+    std::condition_variable m_cond;
 
     void getTask(std::function<void()>& target_callback) {
         std::lock_guard<std::mutex> lock_guard{m_mutex};
@@ -23,8 +24,12 @@ class TaskQueue {
         target_callback = std::move(m_tasks.front());
         m_tasks.pop();
     }
-    static void wait() {
-        std::this_thread::yield();
+    void wait() {
+        std::unique_lock<std::mutex> lk(m_mutex);
+        m_cond.wait(lk);
+    }
+    void wakeUpAll() {
+        m_cond.notify_all();
     }
     void workDone() {
         m_remaining_tasks--;
@@ -34,15 +39,18 @@ public:
     void addTask(TCallback&& callback) {
         std::lock_guard<std::mutex> lock_guard{m_mutex};
         m_tasks.push(std::forward<TCallback>(callback));
+        m_cond.notify_all();
         m_remaining_tasks++;
     }
 
     void waitForCompletion() const {
         while (m_remaining_tasks > 0) {
-            wait();
+            std::this_thread::yield();
         }
     }
     friend Worker;
+    ~TaskQueue() {
+    }
 };
 class Worker
 {
@@ -68,7 +76,7 @@ public:
         while (m_running) {
             m_queue->getTask(m_task);
             if (m_task == nullptr) {
-                TaskQueue::wait();
+                m_queue->wait();
             } else {
                 m_task();
                 m_queue->workDone();
@@ -78,6 +86,7 @@ public:
     }
     void stop() {
         m_running = false;
+        m_queue->wakeUpAll();
         m_thread.join();
     }
 };
